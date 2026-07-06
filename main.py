7e30d5f684634f5e40050cdaacee94bc870e4b77
Rohitdey2697 <rohit.dey2697@gmail.com>
2026-07-06 22:37:44 +0530

    added pydantic to structure the agent output

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
+from typing import List
+from pydantic import BaseModel, Field
 
 load_dotenv()
 
@@ -22,13 +24,37 @@ load_dotenv()
 #     return tavily.invoke({"query": query,"follow_up_questions":1})
 
 
+class Source(BaseModel):
+    """Schema of a source used by Agent"""
+
+    source: str = Field(description="The URL of the source")
+
+
+class AgentResponse(BaseModel):
+    """Schema for Agent rsponse with answer and sources"""
+
+    answer: str = Field(description="The agent answer key")
+    sources: List[Source] = Field(
+        default_factory=list,
+        description="list of sources used by agent to generate answe key",
+    )
+
+
 def main():
     print("Hello from langchain-course!")
 
     llm = ChatGoogleGenerativeAI(model="gemini-3.5-flash")
-    agent = create_agent(model=llm, tools=[TavilySearch()])
+    agent = create_agent(
+        model=llm, tools=[TavilySearch()], response_format=AgentResponse
+    )
     response = agent.invoke(
-        {"messages": [HumanMessage(content="What is the weather in Tokyo?")]}
+        {
+            "messages": [
+                HumanMessage(
+                    content="Find 3 job openings for Gen AI developer in Linkedin"
+                )
+            ]
+        }
     )
     print(response)
 
